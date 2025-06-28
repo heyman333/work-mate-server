4e2595e55d5e7075169cc004814d7094df7db347
lake.zero <lake.zero@kakaostyle.com>
2025-06-29 00:28:30 +0900

    chore: cors host

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://work-mate-web.netlify.app",
     credentials: true,
   })
 );
